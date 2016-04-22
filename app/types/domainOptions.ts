28a0d7aeeafed52dee5737c4d24b80f96eabfe10
Paul Lockwood <dotnetchap@gmail.com>
2016-04-22 15:45:17 -0600

    Moving seed data around

--- app/types/domainOptions.ts
+++ app/types/domainOptions.ts
@@ -1,4 +1,5 @@
 import { DomainOption } from './domainOption';
+import * as SEEDDATA  from '../../../app/seed-data/json-domainOptions';
 
 export class DomainOptions {
   age5: DomainOption[];
@@ -10,4 +11,16 @@ export class DomainOptions {
   yesNo: DomainOption[];
   yesNoDkNaQ1: DomainOption[];
   yesNoDkPnta: DomainOption[];
+
+  populateWithData() {
+    this.age5 = SEEDDATA.AGE_5;
+    this.eduLevel = SEEDDATA.EDU_LEVEL;
+    this.ethnicity = SEEDDATA.ETHNICITY;
+    this.freq2 = SEEDDATA.FREQ_2;
+    this.sex = SEEDDATA.SEX;
+    this.verbalSkill = SEEDDATA.VERBAL_SKILL;
+    this.yesNo = SEEDDATA.YES_NO;
+    this.yesNoDkNaQ1 = SEEDDATA.YES_NO_DK_NA_Q1;
+    this.yesNoDkPnta = SEEDDATA.YES_NO_DK_PNTA;
+  }
 }
