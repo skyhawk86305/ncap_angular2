506f615cb041bf8768e631db1c6488359839cc43
Paul Lockwood <dotnetchap@gmail.com>
2016-05-03 11:05:26 -0600

    Test Page consuming DB JOSN data from service

--- app/services/load.json.data.service.ts
+++ app/services/load.json.data.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Rx';
 
 import { DomainOptions } from  '../../app/types/domain-options';
 import { MatrixElement } from '../../app/types/matrix-element';
 import { Question } from       '../../app/types/question';
-import { Tooltip } from       '../../app/types/tooltip';
 
 import { QUESTIONS_JSON } from '../../app/seed-data/questions.json';
 import { TOOLTIPS_JSON } from '../../app/seed-data/tooltips.json';
 import { MATRIX_ELEMENTS_JSON } from '../../app/seed-data/matrix-elements.json';
 
+import { SurveyPageSre } from '../types/database-data/survey-page-sre';
+import { Subu } from '../types/database-data/subu';
+import { Sre } from '../types/database-data/sre';
+import { Tooltip } from '../types/database-data/tooltip';
+import { DisplayCondition } from '../types/database-data/display-condition';
+import { Domain } from '../types/database-data/domain';
+import { Reference } from '../types/database-data/reference';
+
 import { ValidationService } from './validation.service';
 
 @Injectable()
 export class LoadJsonDataService {
+    display_conditions: DisplayCondition[];
+    domains: Domain[];
+    references: Reference[];
+    sre: Sre[];
+    subu: Subu[];
+    survey_page_sre: SurveyPageSre[];
+    tooltips: Tooltip[];
+
     private _domainOptions: DomainOptions;
     private _questions: Question[];
     private _tooltips: Tooltip[];
     private _matrixElements: MatrixElement[];
 
     constructor(
+        private http: Http,
         private _validationService: ValidationService
     ) {
         this.init();
@@ -32,11 +50,9 @@ export class LoadJsonDataService {
         this._questions = JSON.parse(QUESTIONS_JSON);
         this._tooltips = JSON.parse(TOOLTIPS_JSON);
         this._matrixElements = JSON.parse(MATRIX_ELEMENTS_JSON);
-        
-        // xyzzy5
-        // Create an observable for each file
-        
 
+        // xyzzy5
+        this.readJsonFiles();
     }
 
     getDomainOptions() {
@@ -63,4 +79,44 @@ export class LoadJsonDataService {
     //     return this._matrixElements.filter(i => i.id === id);
     // }
 
+    readJsonFiles() {
+        let that = this;
+        const BASE_PATH = 'app/seed-data/raw-json/';
+        // let temp = this.http.get(BASE_PATH + 'original_survey_metadata.json');
+        // // Call map on the response observable to get the parsed people object
+        // temp.map(res => res.json())
+        //   // Subscribe to the observable to get the parsed people object and attach it to the
+        //   // component
+        //   .subscribe(data => { this.data = data; console.log(data); });
+
+        Observable.forkJoin(
+            this.http.get(BASE_PATH + 'display_conditions.json').map((res: Response) => res.json()),
+            this.http.get(BASE_PATH + 'domains.json').map((res: Response) => res.json()),
+            this.http.get(BASE_PATH + 'references.json').map((res: Response) => res.json()),
+            this.http.get(BASE_PATH + 'sre.json').map((res: Response) => res.json()),
+            this.http.get(BASE_PATH + 'subu.json').map((res: Response) => res.json()),
+            this.http.get(BASE_PATH + 'survey_page_sre.json').map((res: Response) => res.json()),
+            this.http.get(BASE_PATH + 'tooltips.json').map((res: Response) => res.json())
+        ).subscribe(
+            data => {
+                let i = 0;
+                that.display_conditions = data[i++];
+                that.domains = data[i++];
+                that.references = data[i++];
+                that.sre = data[i++];
+                that.subu = data[i++];
+                that.survey_page_sre = data[i++];
+                that.tooltips = data[i++];
+
+                console.log(that.survey_page_sre[6].seq_pag_id);
+                console.log(that.subu[6].subu_sort_order);
+                console.log(that.sre[6].sre_anca_id);
+                console.log(that.tooltips[1].definition);
+                console.log(that.display_conditions[1].relation);
+                console.log(that.domains[3].value);
+                console.log(that.references[3].reference_txt);
+            },
+            err => console.error(err)
+            );
+    }
 }
