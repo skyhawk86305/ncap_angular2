ac0d61d04d1329e830656c4539d98af01bbbcc3b
Paul Lockwood <dotnetchap@gmail.com>
2016-05-03 08:20:53 -0600

    WIP test-page reading all files into class properties (strongly typing is next)

--- app/components/test-page/test-page.component.ts
+++ app/components/test-page/test-page.component.ts
@@ -9,7 +9,17 @@ import {Response} from 'angular2/http';
   templateUrl: 'app/components/test-page/test-page.html'
 })
 export class TestPageComponent implements OnInit {
-  data: any;
+  anca: any;
+  display_conditions: any;
+  domains: any;
+  foca: any;
+  original_survey_metadata: any;
+  references: any;
+  sre: any;
+  subu: any;
+  survey_metadata: any;
+  survey_page_sre: any;
+  tooltips: any;
 
   constructor(
     private http: Http) {
@@ -21,12 +31,12 @@ export class TestPageComponent implements OnInit {
 
   readJsonFiles() {
     const BASE_PATH = 'app/seed-data/raw-json/';
-    let temp = this.http.get(BASE_PATH + 'original_survey_metadata.json');
-    // Call map on the response observable to get the parsed people object
-    temp.map(res => res.json())
-      // Subscribe to the observable to get the parsed people object and attach it to the
-      // component
-      .subscribe(data => { this.data = data; console.log(data); });
+    // let temp = this.http.get(BASE_PATH + 'original_survey_metadata.json');
+    // // Call map on the response observable to get the parsed people object
+    // temp.map(res => res.json())
+    //   // Subscribe to the observable to get the parsed people object and attach it to the
+    //   // component
+    //   .subscribe(data => { this.data = data; console.log(data); });
 
     Observable.forkJoin(
       this.http.get(BASE_PATH + 'anca.json').map((res: Response) => res.json()),
@@ -42,28 +52,29 @@ export class TestPageComponent implements OnInit {
       this.http.get(BASE_PATH + 'tooltips.json').map((res: Response) => res.json())
     ).subscribe(
       data => {
-        this.data = data[0];
-        console.log(this.data);
-        this.data = data[1];
-        console.log(this.data);
-        this.data = data[2];
-        console.log(this.data);
-        this.data = data[3];
-        console.log(this.data);
-        this.data = data[4];
-        console.log(this.data);
-        this.data = data[5];
-        console.log(this.data);
-        this.data = data[6];
-        console.log(this.data);
-        this.data = data[7];
-        console.log(this.data);
-        this.data = data[8];
-        console.log(this.data);
-        this.data = data[9];
-        console.log(this.data);
-        this.data = data[10];
-        console.log(this.data);
+        this.anca = data[0];
+        this.display_conditions = data[1];
+        this.domains = data[2];
+        this.foca = data[3];
+        this.original_survey_metadata = data[4];
+        this.references = data[5];
+        this.sre = data[6];
+        this.subu = data[7];
+        this.survey_metadata = data[8];
+        this.survey_page_sre = data[9];
+        this.tooltips = data[10];
+
+        console.log(this.anca);
+        console.log(this.display_conditions);
+        console.log(this.domains);
+        console.log(this.foca);
+        console.log(this.original_survey_metadata);
+        console.log(this.references);
+        console.log(this.sre);
+        console.log(this.subu);
+        console.log(this.survey_metadata);
+        console.log(this.survey_page_sre);
+        console.log(this.tooltips);
       },
       err => console.error(err)
       );
