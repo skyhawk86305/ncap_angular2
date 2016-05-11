0b1a9f0aca92a07d1154a8430bbf9dc70af2626a
Henry Nguyen <st8li@rice.edu>
2016-05-11 11:33:10 -0500

    Add data and types for Address_Inputs and Error_Msg tables

---
+++ app/types/database-data/adin.ts
@@ -0,0 +1,16 @@
+export class Adin {
+  obj_uid: number;
+  sort_order: number;
+  display_condition: string;
+  adin_foca_id: number;
+  adin_anca_id: number;
+  bypass_property: string;
+  txt_parent_lang1: string;
+  txt_legalrep_lang1: string;
+  txt_selfreport_lang1: string;
+  parent_adin_dona_id: number;
+  legalrep_adin_dona_id  : number;
+  selfreport_adin_dona_id: number;
+  tracking_key: string;
+  bypass_var: string;
+}
---
+++ app/types/database-data/error-msg.ts
@@ -0,0 +1,6 @@
+export class ErrorMsg {
+  id: number;
+  txt_parent_lang1: string;
+  txt_legalrep_lang1: string;
+  txt_selfreport_lang1: string;
+}
