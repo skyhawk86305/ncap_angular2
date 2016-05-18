894eea917179ecd20d0852270579ad9aa52c36c3
Paul Lockwood <dotnetchap@gmail.com>
2016-05-18 14:15:54 -0600

    Revert "Add page_type to Page type"
    This reverts commit 388148ef6d16739a2dd96396aab98ef78a061152.

--- app/types/database-data/page.ts
+++ app/types/database-data/page.ts
@@ -1,8 +1,6 @@
 export class Page {
   constructor(
     public page_id: number,
-    public page_sort_order: number,
-    public page_type: number
-  ){  }  
-
+    public page_sort_order: number
+  ) {}  
 }
