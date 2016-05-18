a6a0d33b7fe9875bd41d9d9065ad1a3cfbe5254c
Paul Lockwood <dotnetchap@gmail.com>
2016-05-18 12:27:48 -0600

    Added page-type.enum and got page etc working

--- app/types/database-data/page.ts
+++ app/types/database-data/page.ts
@@ -1,8 +1,9 @@
+import { PageType } from '../types/enums/page-type';
+
 export class Page {
   constructor(
     public page_id: number,
     public page_sort_order: number,
-    public page_type: number
-  ){  }  
-
+    public page_type: PageType
+  ) { }
 }
---
+++ app/types/enums/page-type.enum.ts
@@ -0,0 +1,11 @@
+export enum PageType {
+    normal = 1,
+    home = 2,
+    consent = 3,
+    birthCertificate = 4,
+    residentialBlockPart1 = 5,
+    residentialBlockPart2 = 6,
+    residentialBlockPart3 = 7,
+    reContact = 8,
+    lastPage = 9
+}
