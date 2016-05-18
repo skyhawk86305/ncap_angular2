8d04704d15bf7a53693ceb4e5fd7eb2264838398
Paul Lockwood <dotnetchap@gmail.com>
2016-05-18 14:15:41 -0600

    Revert "Added page-type.enum and got page etc working"
    This reverts commit a6a0d33b7fe9875bd41d9d9065ad1a3cfbe5254c.

--- app/types/database-data/page.ts
+++ app/types/database-data/page.ts
@@ -1,9 +1,8 @@
-import { PageType } from '../types/enums/page-type';
-
 export class Page {
   constructor(
     public page_id: number,
     public page_sort_order: number,
-    public page_type: PageType
-  ) { }
+    public page_type: number
+  ){  }  
+
 }
