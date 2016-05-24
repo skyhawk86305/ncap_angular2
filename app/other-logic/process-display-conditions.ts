a24a813ef903edf684b9a97bd2f98ab1b9557bb7
Paul Lockwood <dotnetchap@gmail.com>
2016-05-24 13:31:39 -0600

    Started refatoring ProcessDisplayCondition logic

--- app/other-logic/process-display-conditions.ts
+++ app/other-logic/process-display-conditions.ts
@@ -7,27 +7,44 @@ export class ProcessDisplayCondition {
 
     static processDisplayCondition(pageQuestion: PageQuestion): boolean {
         let result = false;
-        console.log('parent_sre_disp_id is ' + pageQuestion.parent_sre_disp_id + ' for pg' + pageQuestion.page_id + '-qu' + pageQuestion.sre_sort_order);
+        console.log('parent_sre_disp_id is ' + pageQuestion.parent_sre_disp_id
+            + ' for pg' + pageQuestion.page_id + '-qu' + pageQuestion.sre_sort_order);
 
         let displayConditions: DisplayCondition[] = displayConditionDict[pageQuestion.parent_sre_disp_id];
 
         for (let curDisplayCondition of displayConditions) {
-            if (curDisplayCondition.relation === 'EQUAL') {
-                //console.log('EQUAL tracking_key ' + curDisplayCondition.tracking_key);
-                let userInput = UserInputSingleton.instanceOf().getUserInput(curDisplayCondition.tracking_key);
-                if (userInput) {
-                    let expectedstoredValue: number = curDisplayCondition.stored_value;
-                    let expectedDisplayValue = curDisplayCondition.displayed_value;
-                    //console.log(' DisplayCondition.stored_value is ' + curDisplayCondition.stored_value + ',' + 'DisplayCondition.displayed_value is ' + curDisplayCondition.displayed_value);
-                    result = (+expectedstoredValue === +userInput.storedValue);
-                    result = result || (expectedDisplayValue === userInput.storedValue);
-                }
-            }
+            result = ProcessDisplayCondition.evaluteDisplyCondition(curDisplayCondition);
         }
 
         //console.log('result is ' + result);
 
         return result;
     }
+
+    static evaluteDisplyCondition(curDisplayCondition: DisplayCondition) {
+        let result = false;
+
+        if (curDisplayCondition.relation === 'EQUAL') {
+            // console.log('EQUAL tracking_key ' + curDisplayCondition.tracking_key);
+            let userInput = UserInputSingleton.instanceOf().getUserInput(curDisplayCondition.tracking_key);
+            if (userInput) {
+                let expectedstoredValue: number = curDisplayCondition.stored_value;
+                let expectedDisplayValue = curDisplayCondition.displayed_value;
+                // console.log(' DisplayCondition.stored_value is ' + curDisplayCondition.stored_value 
+                // + ',' + 'DisplayCondition.displayed_value is ' + curDisplayCondition.displayed_value);
+                result = (+expectedstoredValue === +userInput.storedValue);
+                result = result || (expectedDisplayValue === userInput.storedValue);
+            }
+        }
+
+        return result;
+    }
+
 }
 
+// EQUAL 
+// CONTAINS  
+// IS NOT NULL 
+// NOT EQUAL
+// IS NULL
+// GREATER THAN
