68bdd7d663cbf72ffd5c9c8e9c91d1ba08dcf181
Paul Lockwood <dotnetchap@gmail.com>
2016-05-24 18:20:53 -0600

    Scenario 1 default to page 10 (a matric page) + fixed most TSLint issues with scanrios json

--- app/seed-data/json-for-debugging/user-input-senario2.ts
+++ app/seed-data/json-for-debugging/user-input-senario2.ts
@@ -1,4 +1,4 @@
 import { UserInput } from  '../../../app/types/user-input';
 
 export var USERINPUT_SCENARIO2: UserInput[] =
-    [{ "trackingKey": "respondent_type", "storedValue": "parent" }, { "trackingKey": "initial_consent", "storedValue": "Y" }, { "trackingKey": "asd_yes_no", "storedValue": "2" }, { "trackingKey": "proband_DOB", "storedValue": "2004-05-03" }, { "trackingKey": "race", "storedValue": "3" }, { "trackingKey": "sex_at_birth", "storedValue": "2" }, { "trackingKey": "ethnicity", "storedValue": "2" }, { "trackingKey": "weight_std", "storedValue": "55" }];
+    [{ 'trackingKey': 'respondent_type', 'storedValue': 'parent' }, { 'trackingKey': 'initial_consent', 'storedValue': 'Y' }, { 'trackingKey': 'asd_yes_no', 'storedValue': '2' }, { 'trackingKey': 'proband_DOB', 'storedValue': '2004-05-03' }, { 'trackingKey': 'race', 'storedValue': '3' }, { 'trackingKey': 'sex_at_birth', 'storedValue': '2' }, { 'trackingKey': 'ethnicity', 'storedValue': '2' }, { 'trackingKey': 'weight_std', 'storedValue': '55' }];
