0019067f6c964d4266821c95aa9b8fb4255fbfdd
Paul Lockwood <dotnetchap@gmail.com>
2016-05-31 15:52:49 -0600

    SubuQuestion parent_sre_varu_id, legalrep_sre_varu_id and selfreport_sre_varu_id are now private

--- app/types/database-data/subu-question.ts
+++ app/types/database-data/subu-question.ts
@@ -31,9 +31,9 @@ export class SubuQuestion {
     private selfreport_sre_dona_id: number,
     public tracking_key: string,
     public def_disp_value: string,
-    public parent_sre_varu_id: number,
-    public legalrep_sre_varu_id: number,
-    public selfreport_sre_varu_id: number,
+    private parent_sre_varu_id: number,
+    private legalrep_sre_varu_id: number,
+    private selfreport_sre_varu_id: number,
     public error_msg_lang1: number,
     public bypass_var: string
   ) { }
