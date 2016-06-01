04ad185e3c8a731f55bdb1cc2c3351d456fc2fca
Paul Lockwood <dotnetchap@gmail.com>
2016-06-01 10:14:06 -0600

    Made Page class use PageType enum

--- app/types/database-data/page.ts
+++ app/types/database-data/page.ts
@@ -1,9 +1,11 @@
+import { PageType } from '../../types/enums/page-type.enum';
+
 export class Page {
   constructor(
     public page_number: number,
     public page_id: number,
     public page_sort_order: number,
-    public page_type: number
+    public page_type: PageType
   ) { }
 
 }
