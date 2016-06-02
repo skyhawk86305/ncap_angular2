0f94286b1e40ca31ecbabf148d4a530cc41ae9fa
Paul Lockwood <dotnetchap@gmail.com>
2016-06-01 18:14:46 -0600

    Checkpoint: removing page_number (field gone from page.ts and seed data now + app still seems to be working

--- app/types/database-data/page.ts
+++ app/types/database-data/page.ts
@@ -2,7 +2,6 @@ import { PageType } from '../../types/enums/page-type.enum';
 
 export class Page {
   constructor(
-    private page_number: number,
     public page_id: number,
     public page_sort_order: number,
     public page_type: PageType
